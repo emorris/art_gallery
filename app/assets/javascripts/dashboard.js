b72728354547597e5102d46a8e115ab49442d38c
Evan Morris <evanm978@gmail.com>
2016-01-03 09:44:25 -0800

    few more angular parts

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -6,7 +6,8 @@
     [
       'ngRoute',
       'templates',
-      'ngSanitize'
+      'ngSanitize',
+      'ngResource',
     ]
   )
 
@@ -22,22 +23,82 @@
       $routeProvider.when("/", {
         controller: "overviewController",
         templateUrl: "overview.html"
+      }).when("/gallery/:id", {
+        controller: "galleryController",
+        templateUrl: "gallery.html"
+      }).when("/about", {
+        controller: "aboutController",
+        templateUrl: "about.html"
+      }).when("/news", {
+        controller: "newsController",
+        templateUrl: "news.html"
+      }).when("/artists", {
+        controller: "artistsController",
+        templateUrl: "artists.html"
       })
     }
   ]);
 
-  app.controller("overviewController", [ 
-            "$scope","$http","$routeParams",
-    function($scope , $http , $routeParams) {
-      $scope.galleries =[]
-      $http.get("/galleries.json").success(
-        function(data) { 
-          $scope.galleries = data;
-          console.log($scope.galleries)
-      }).error(
-        function(data, status ,headers, config) {
-          alert("There was a problem: " + status);
+ app.factory('galleryFactory',  ['$http','$resource', function($http, $resource){
+    var Gallery = $resource('/galleries/:id.json')
+      Gallery.getAll =  function(callback){
+          return $http.get("/galleries.json",{cache: true}).success(
+          function(data) { 
+            callback(data);
+        }).error(
+          function(data, status ,headers, config) {
+            alert("There was a problem: " + status);
         });
+      }
+      return Gallery
+  }]);
+
+  app.controller("overviewController", [ 
+            "$scope","$http","galleryFactory",
+    function($scope, $http, galleryFactory) {
+      $scope.galleries =[]
+      galleryFactory.getAll(function(data){
+        $scope.galleries = data
+      })
     }
   ]);
+
+  app.controller("galleryController",["$scope","$routeParams","galleryFactory",
+    function($scope, $routeParams, galleryFactory) {
+      $scope.gallery = galleryFactory.get({ "id": $routeParams.id})
+    }
+  ])
+
+  app.controller("artistsController",["$scope","$routeParams",
+    function($scope, $routeParams) {
+    }
+  ])
+
+  app.controller("newsController",["$scope","$routeParams",
+    function($scope, $routeParams) {
+    }
+  ])
+
+  app.controller("aboutController",["$scope","$routeParams",
+    function($scope, $routeParams) {
+    }
+  ])
+
+  app.controller("navController", [ "$scope","$http","$location","galleryFactory", 
+    function($scope, $http, $location, galleryFactory) {
+      $scope.galleries =[]
+      
+      galleryFactory.getAll(function(data){
+        $scope.galleries = data
+      })
+
+      $scope.onClickGallery = function(gallery_id){
+        $location.path("/gallery/" + gallery_id);
+      }
+
+      $scope.onClickBottom = function(name){
+        $location.path("/"+name);
+      }
+
+  }])
 })()
